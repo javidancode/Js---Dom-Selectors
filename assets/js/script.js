53fe2ba3a20af688a74b4ecc89b0bc6099a6b61d
javidancode <javidanig@code.edu.az>
2022-08-29 07:47:32 +0400

    Added Js - Dom , Selectors

---
+++ assets/js/script.js
@@ -0,0 +1,185 @@
+// console.log(document);
+
+// let heads = document.getElementsByTagName("h3");
+
+// for (const item of heads) {
+//     console.log(item);
+// }
+
+
+// let head = document.getElementsByClassName("test");
+// console.log(head);
+
+// let head = document.getElementById("test");
+// console.log(head);
+
+// let head = document.querySelector(".test");
+// let heads = document.querySelectorAll(".test");
+// console.log(head);
+
+// for (const item of heads) {
+//     console.log(item);
+// }
+
+// console.log(document.querySelector("#list-news .test ul"));
+
+
+// let elem = document.querySelector(".test");
+// elem.className = "salamlar"
+
+// elem.classList.add("salam");
+
+// elem.classList.remove("test1");
+// elem.classList.add("salam");
+
+// console.log(elem);
+
+// if (elem.classList.contains("test1")) {
+//     elem.classList.remove("test1");
+// }else{
+//     console.log("yoxdur");
+// }
+
+
+// let span = document.querySelector("#list-news .small-text");
+// console.log(span.innerText);
+// console.log(span.innerHTML);
+
+// span.innerText = "<b>Salam P130</b>";
+// span.innerHTML = "<b>Salam P130</b>";
+
+
+// let addBtn = document.querySelector(".add-btn");
+
+// function clickBtn(){
+//     alert("Salam millet");
+// }
+
+// addBtn.onclick = function(){
+//     alert("Added");
+// }
+
+// addBtn.onclick = function(){
+//     alert("Salam");
+// }
+
+// function test() {
+//     console.log("salamlar");
+// }
+
+// addBtn.addEventListener("click",test);
+
+
+// addBtn.addEventListener("click", function (){
+//     alert("salamlar");
+// })
+
+// addBtn.addEventListener("click",function(){
+//     alert("sagollar");
+// })
+
+
+// let addBtn = document.querySelector(".add-btn");
+
+// let removeBtn = document.querySelector(".remove-btn");
+
+// let text = document.querySelector(".text-area");
+
+// let head = document.querySelector("h1");
+
+
+// addBtn.addEventListener("click", function () {
+//         head.innerText = text.value;
+//         text.value = "";
+// })
+
+// addBtn.addEventListener("click", function () {
+//     if (text.value == "") {
+//         alert("Bosh buraxma")
+//         return;
+//     }
+
+//     head.innerText = text.value;
+//     if (head.classList.contains("active-head")) {
+//         head.classList.remove("active-head");
+//         head.classList.add("inActive-head");
+//     }
+//     text.value = "";
+// })
+
+// removeBtn.addEventListener("click", function () {
+//     if (text.value == "") {
+//         alert("Bosh buraxma")
+//         return;
+//     }
+
+//     head.innerText = text.value;
+//     if (head.classList.contains("inActive-head")) {
+//         head.classList.remove("inActive-head");
+//         head.classList.add("active-head");
+//     }
+//     text.value = "";
+// })
+
+
+// addBtn.addEventListener("click", function () {
+//     console.log(tested);
+//     text.value = "";
+// })
+
+// let addBtn = document.querySelector(".add-btn");
+
+// let removeBtn = document.querySelector(".remove-btn");
+
+// let text = document.querySelector(".text-area");
+
+// let head = document.querySelector("h1");
+
+// let ul = document.querySelector(".list-area");
+
+
+
+// addBtn.addEventListener("click", function () {
+
+//     if (text.value == "") {
+//         alert("Bosh buraxma")
+//         return;
+//     }
+
+//     // let li = document.createElement("li");
+
+//     // li.classList.add("list-group-item");
+//     // li.classList.add("w-25");
+//     // li.style.border = "2px solid red";
+//     // li.style.backgroundColor = "yellow";
+//     // li.style.cursor = "pointer";
+
+//     // li.style = {"border":"1px solid blue"};
+//     // li.innerText = text.value;
+//     // ul.append(li);
+
+//     // li.onclick = function(){
+//     //     this.remove();
+//     // }
+    
+
+//     // li.innertext = text.value;
+
+//     // ul.append(li);
+
+//     // ul.innerHTML += `<li class = "list-group-item">${text.value}</li>`
+
+//     // console.log(this);
+
+
+
+//     text.value = "";
+// })
+
+// window.onload(function(){
+    
+// })
+
+// window.addEventListener("load",(event) =>{
+//     console.log("eminsinizmi?");
+// });
